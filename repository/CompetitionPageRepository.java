4bc4c3cc22a82f0997c000bf5200fc58040d8f80
coalsld <152403126+coalsld@users.noreply.github.com>
2024-11-29 11:24:16 +0900

    Delete repository directory

--- repository/CompetitionPageRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.joinup.competitionboard.repository;
-
-import com.example.joinup.competitionboard.entity.CompetitionPage;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface CompetitionPageRepository extends JpaRepository<CompetitionPage, Integer> {
-}
