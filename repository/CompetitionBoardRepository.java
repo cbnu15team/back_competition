4ac1681786e087de6c566b2c9e696452987e39f2
coalsld <152403126+coalsld@users.noreply.github.com>
2024-12-02 21:08:53 +0900

    Delete repository/CompetitionBoardRepository.java

--- repository/CompetitionBoardRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.joinup.competitionboard.repository;
-
-import com.example.joinup.competitionboard.entity.CompetitionBoard;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface CompetitionBoardRepository extends JpaRepository<CompetitionBoard, Integer> {
-}
