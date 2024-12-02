1030691a10c275b7aff36ec40073f33811c65a40
coalsld <152403126+coalsld@users.noreply.github.com>
2024-12-02 21:09:03 +0900

    Update CompetitionPageRepository.java

--- repository/CompetitionPageRepository.java
+++ repository/CompetitionPageRepository.java
@@ -2,8 +2,6 @@ package com.example.joinup.competitionboard.repository;
 
 import com.example.joinup.competitionboard.entity.CompetitionPage;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
-@Repository
-public interface CompetitionPageRepository extends JpaRepository<CompetitionPage, Integer> {
+public interface CompetitionPageRepository extends JpaRepository<CompetitionPage, Long> {
 }
